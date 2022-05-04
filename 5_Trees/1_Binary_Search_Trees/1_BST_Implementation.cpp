2b24788d44ccbcda0f62767baf2b8dd7bbcc45f1
Nishit Jain <nishitjain@Nishits-MacBook-Pro.local>
2022-05-04 21:21:45 +0530

    init: This commit will add initial code for BST implementation.

---
+++ 5_Trees/1_Binary_Search_Trees/1_BST_Implementation.cpp
@@ -0,0 +1,21 @@
+/*
+Title: BST Implementation
+
+Problem Type: BST
+
+Description: Implement Binary Search Tree.
+*/
+#include<iostream>
+using namespace std;
+
+struct BST {
+    int data;
+    struct BST *left;
+    struct BST *right;
+};
+
+
+
+int main() {
+    return 0;
+}
